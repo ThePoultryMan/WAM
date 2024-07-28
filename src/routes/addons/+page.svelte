e854ee128a59ad44107ed64d8eddcfce84443123
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-28 14:42:56 -0400

    feat: addon page

---
+++ src/routes/addons/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import DynamicHR from "$components/DynamicHR.svelte";
+
+  let page = 0;
+
+  let headerElement;
+</script>
+
+<div class="flex w-full">
+  <div class="flex-1">
+    <h1 bind:this={headerElement} class="w-fit text-2xl">Addons</h1>
+    <DynamicHR element={headerElement} />
+    {#if page === 0}
+    {:else}
+      
+    {/if}
+  </div>
+  <div class="flex flex-col items-center w-1/6">
+    <button on:click={() => page = 1} class="w-5/6 px-2 py-1 bg-neutral-900 border border-neutral-300">
+      Add
+    </button>
+  </div>
+</div>
